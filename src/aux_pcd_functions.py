f0ab9b1ea3fb4b2349fadce51ec2a872ec557f44
Stefano Ceolin <stefanoceo@gmail.com>
2023-07-06 12:26:03 +0200

    updated: option to upscale image when converting to a pcd

--- src/aux_pcd_functions.py
+++ src/aux_pcd_functions.py
@@ -37,53 +37,78 @@ def pcd_to_image(pcd, pcd_values, image_shape):
     image = np.zeros(image_shape)
     points_count = np.zeros(image_shape)
     assert len(pcd_values) == pcd_array.shape[-1],\
-           " ERROR: the number of points in the pcd object doesn't match the number of britghness values."
+        " ERROR: the number of points in the pcd object doesn't match the number of britghness values."
 
     index_error_flag = False
-    
+
     for i in range(pcd_array.shape[-1]):
-        pos = tuple(pcd_array[...,i])
+        pos = tuple(pcd_array[..., i])
         try:
             image[pos] += pcd_values[i]
             points_count[pos] += 1
         except IndexError:
             index_error_flag = True
-            
+
     if index_error_flag:
         print("Warning: The point cloud does not fit the image shape. Part of the object will be cropped.",  file=sys.stderr)
 
     # average the birghtness in voxels containing multiple points:
-    image[points_count>1] = image[points_count>1]/points_count[points_count>1]
+    image[points_count > 1] = image[points_count > 1] / \
+        points_count[points_count > 1]
 
     # fill holes with the median of neighbouring voxels:
-    mask = morphology.closing(image>0, morphology.ball(2))
+    mask = morphology.closing(image > 0, morphology.ball(2))
     image_median = ndimage.median_filter(image, size=3)
-    image[points_count==0] = image_median[points_count==0]
+    image[points_count == 0] = image_median[points_count == 0]
     image = image*mask
     return image
 
 
-def image_to_pcd(image_3d):
+def image_to_pcd(image_3d, upscale=None):
     """
+    Convert a 3D image into a point cloud and an array of brightness values of each point.
+
     Parameters
     ----------
-    image_3d : 3 dimensional numpy array
-        the input (sparse) 3d image.
+    image_3d : numpy.ndarray
+        The input (sparse) 3D image.
+
+    upscale : float or None, optional
+        Upscaling factor for the image. If provided, the image will be zoomed by the given factor.
+        Default is None.
 
     Returns
     -------
-    pcd : open3d pcd.
-        an open3d point cloud object.
-    pcd_values : 1D numpy array
-        brightness of the point cloud points.
+    pcd : open3d.geometry.PointCloud
+        An open3d point cloud object.
 
-    This function converts a 3d image into a point cloud and an array of brightness
-    values of each point.
+    pcd_values : numpy.ndarray
+        1D array containing the brightness values of the point cloud points.
 
+    Notes
+    -----
+    If `upscale` is provided, the `image_3d` will be zoomed by the specified factor before converting
+    it into a point cloud. The zoomed image will be used to generate the point cloud and brightness
+    values.
+
+    If `upscale` is not provided, the original `image_3d` will be used directly to generate the point
+    cloud and brightness values.
+
+    The point cloud coordinates are computed from the non-zero elements of `image_3d` using the
+    `np.nonzero` function. The resulting point cloud is stored in an `open3d.geometry.PointCloud`
+    object.
     """
-    indexes = np.nonzero(image_3d > 0)
-    pcd_points = np.array(indexes).T
-    pcd_values = image_3d[indexes]
+
+    if upscale:
+        upscaled_image = ndimage.zoom(image_3d, upscale)
+        indexes = np.nonzero(upscaled_image > 0)
+        pcd_points = np.array(indexes).T/upscale
+        pcd_values = upscaled_image[indexes]
+    else:
+        indexes = np.nonzero(image_3d > 0)
+        pcd_points = np.array(indexes).T
+        pcd_values = image_3d[indexes]
+
     pcd = o3d.geometry.PointCloud()
     pcd.points = o3d.utility.Vector3dVector(pcd_points)
 
